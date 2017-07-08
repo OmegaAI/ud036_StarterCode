6a7c6f52386a5319907ea60950603c23634af138
unknown <omegaaicoding@gmail.com>
2017-07-08 12:09:52 -0500

    Insert open movie function

--- entertainment.py
+++ entertainment.py
@@ -1,6 +1,7 @@
+import fresh_tomatoes
 import media
 
-# Create list of movie objects
+# List of movie titles with poster image and trailer
 
 the_other_guys = the_other_guys.Movie("The Other Guys", "http://www.imdb.com/title/tt1386588/mediaviewer/rm1289719040", "https://www.youtube.com/watch?v=D6WOoUG1eNo")
 
@@ -13,3 +14,8 @@ american_psycho = american_psycho.Movie("American Psycho", "http://t1.gstatic.co
 office_space = office_space.Movie("Office Space", "http://www.gstatic.com/tv/thumb/movieposters/22554/p22554_p_v8_aa.jpg", "https://www.youtube.com/watch?v=dMIrlP61Z9s")
 
 die_hard = die_hard.Movie("Die Hard", "http://www.gstatic.com/tv/thumb/movieposters/10255/p10255_p_v8_ar.jpg", "https://www.youtube.com/watch?v=2TQ-pOvI6Xo")
+
+
+movies = [the_other_guys, star_wars_ep3, step_brothers, american_psycho, office_space, die_hard]
+
+fresh_tomatoes.open_movies_page(movies)
