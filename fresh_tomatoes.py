c34e62faa7bce3abc5587acbb0670cef0a4bf601
unknown <omegaaicoding@gmail.com>
2017-07-08 12:27:17 -0500

    Customize titles

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -9,7 +9,7 @@ main_page_head = '''
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>Favorite Movies</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -107,12 +107,12 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Favorite Movies Movie Trailers</a>
           </div>
         </div>
       </div>
     </div>
-    <div class="container">
+    <div class="container-fluid">
       {movie_tiles}
     </div>
   </body>
