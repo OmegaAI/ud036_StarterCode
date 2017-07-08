17639c32d3ebf9f55b2b9d5d2fbc9e9bcece0550
unknown <omegaaicoding@gmail.com>
2017-07-08 12:05:13 -0500

    Add function to show trailer

--- media.py
+++ media.py
@@ -1,6 +1,11 @@
+import webbrowser
+
 class Movie():
 	
 	def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
 		self.title = movie_title
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
+
+	def showtrailer(self):
+		webbrowser.open(self.trailer)
