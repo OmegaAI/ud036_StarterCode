0a34fffd305bc66d32ec933a8640419a29eeee89
unknown <omegaaicoding@gmail.com>
2017-07-15 13:03:12 -0500

    Clean code to PEP 8

--- entertainment.py
+++ entertainment.py
@@ -1,21 +1,42 @@
 import fresh_tomatoes
 import media
 
-# Assign movie titles with poster image and trailer. Create instance of Movie class for each title.
+#Assign movie titles with poster image and trailer. 
+#Create instance of Movie class for each title.
+the_other_guys = media.Movie(
+	"The Other Guys",
+	"http://img.moviepostershop.com/the-other-guys-movie-poster-2010-1020545762.jpg",
+	"https://www.youtube.com/watch?v=D6WOoUG1eNo")
 
-the_other_guys = media.Movie("The Other Guys", "http://img.moviepostershop.com/the-other-guys-movie-poster-2010-1020545762.jpg", "https://www.youtube.com/watch?v=D6WOoUG1eNo")
+star_wars_ep3 = media.Movie(
+	"Star Wars Episode 3",
+	"http://t0.gstatic.com/images?q=tbn:ANd9GcT7TlfhiJ93841oYulGpyJZ3YULcgzah1CGumaVQuZ3-zXarfai",
+	"https://www.youtube.com/watch?v=MR1xo1tLyBk")
 
-star_wars_ep3 = media.Movie("Star Wars Episode 3", "http://t0.gstatic.com/images?q=tbn:ANd9GcT7TlfhiJ93841oYulGpyJZ3YULcgzah1CGumaVQuZ3-zXarfai", "https://www.youtube.com/watch?v=MR1xo1tLyBk")
+step_brothers = media.Movie(
+	"Step Brothers",
+	"http://www.gstatic.com/tv/thumb/movieposters/175884/p175884_p_v8_ag.jpg",
+	"https://www.youtube.com/watch?v=ANjenc4W1_Q")
 
-step_brothers = media.Movie("Step Brothers", "http://www.gstatic.com/tv/thumb/movieposters/175884/p175884_p_v8_ag.jpg", "https://www.youtube.com/watch?v=ANjenc4W1_Q")
+american_psycho = media.Movie(
+	"American Psycho",
+	"http://t1.gstatic.com/images?q=tbn:ANd9GcR42NIX4d1J7ehsoZ9jAQoI04YS9Cz0cohVJvKkKLo7PVz7jj1B",
+	"https://www.youtube.com/watch?v=5YnGhW4UEhc")
 
-american_psycho = media.Movie("American Psycho", "http://t1.gstatic.com/images?q=tbn:ANd9GcR42NIX4d1J7ehsoZ9jAQoI04YS9Cz0cohVJvKkKLo7PVz7jj1B", "https://www.youtube.com/watch?v=5YnGhW4UEhc")
+office_space = media.Movie(
+	"Office Space",
+	"http://www.gstatic.com/tv/thumb/movieposters/22554/p22554_p_v8_aa.jpg",
+	"https://www.youtube.com/watch?v=dMIrlP61Z9s")
 
-office_space = media.Movie("Office Space", "http://www.gstatic.com/tv/thumb/movieposters/22554/p22554_p_v8_aa.jpg", "https://www.youtube.com/watch?v=dMIrlP61Z9s")
+die_hard = media.Movie(
+	"Die Hard",
+	"http://www.gstatic.com/tv/thumb/movieposters/10255/p10255_p_v8_ar.jpg",
+	"https://www.youtube.com/watch?v=2TQ-pOvI6Xo")
 
-die_hard = media.Movie("Die Hard", "http://www.gstatic.com/tv/thumb/movieposters/10255/p10255_p_v8_ar.jpg", "https://www.youtube.com/watch?v=2TQ-pOvI6Xo")
+#Create list of movies that will be opened and displayed by fresh tomatoes.
+movies = [
+	the_other_guys, star_wars_ep3, step_brothers,
+	american_psycho, office_space, die_hard] 
 
-
-movies = [the_other_guys, star_wars_ep3, step_brothers, american_psycho, office_space, die_hard] #Create list of movies that will be opened and displayed by fresh tomatoes.
-
-fresh_tomatoes.open_movies_page(movies)  #Opens movies with function called from fresh tomatoes
+#Open movies with function called from fresh tomatoes.
+fresh_tomatoes.open_movies_page(movies)  
--- media.py
+++ media.py
@@ -3,11 +3,11 @@ import webbrowser
 class Movie():
 	
 	def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
-		# Init function for Movie class.
+		"""Init function for Movie class."""
 		self.title = movie_title
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
 
-	def showtrailer(self):
-		#Use webbrowser library to open the url and play movie trailer
+	def show_trailer(self):
+		"""Use webbrowser library to open the url and play movie trailer."""
 		webbrowser.open(self.trailer)
